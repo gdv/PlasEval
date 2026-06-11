d7432daff7c430f0efcc1e519cb421e2d2281238
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-06-11 17:31:55 +0200

    chach length computation
    We try to reduce the number of calls needed to compute the contig lenths
    
    Signed-off-by: Gianluca Della Vedova <gianluca@dellavedova.org>

--- src/compare_sets.py
+++ src/compare_sets.py
@@ -42,28 +42,34 @@ def get_matching_positions(ctg_copies, matching):
 		r_posn.append(R[x])	
 	return l_posn, r_posn
 
-def rename_by_matching(matching_dict):
+def rename_by_matching(matching_dict, contigs_dict):
 	'''
 	Input: 
 		Dictionary of matchings: Key: contig, 
 								 Value: matching for copies of contig
 								 Each matching is pair of lists of indices (int) of the contig copies, one for each side
+		contigs_dict: Key: contig (str), Value: Nested dictionary with 'length' (int)
 	Returns:
 	 	Two lists of contig copies, one for each plasmid set, renamed with indices according to the input matching 
 		Each contig is a triple [contig, plasmid index (int), position in plasmid (int)]
+		ctg_len: dict mapping each renamed contig to its length (avoids split('_') in get_partition_cost)
 	'''
 	reached_contigs = matching_dict.keys()
 	left_copies_renamed, right_copies_renamed = [], []
+	ctg_len = {}
 	for contig in reached_contigs:
 		M = matching_dict[contig]
 		left_ctgs = M[0]
 		right_ctgs = M[1]
+		ctg_length = contigs_dict[contig]['length']
 		for i in range(len(M[0])):
+			renamed = contig+'_'+str(i)
+			ctg_len[renamed] = ctg_length
 			lpls, lidx = left_ctgs[i][1], left_ctgs[i][2]
-			left_copies_renamed.append([contig+'_'+str(i), lpls, lidx])
+			left_copies_renamed.append([renamed, lpls, lidx])
 			rpls, ridx = right_ctgs[i][1], right_ctgs[i][2]
-			right_copies_renamed.append([contig+'_'+str(i), rpls, ridx])
-	return left_copies_renamed, right_copies_renamed
+			right_copies_renamed.append([renamed, rpls, ridx])
+	return left_copies_renamed, right_copies_renamed, ctg_len
 
 def add_nodes(G, left_ctg_list, right_ctg_list, pls_ids_dict):
 	'''
@@ -133,12 +139,12 @@ def modify_partitions(partitions, common):
 			modified_partitions.append(S)	
 	return modified_partitions		
 
-def get_partition_cost(partitions, contigs_dict, p):
+def get_partition_cost(partitions, ctg_len, p):
 	'''
 	Input:
-		partitions: List of sets of contigs
-		contigs_dict: Key: contig (str), Value: Nested dictionary: 	length (int), 
-																	L_copies/R_copies (list of contig copies in plasmid set)
+		partitions: List of sets of contigs (renamed, e.g. "C1_0")
+		ctg_len: dict mapping renamed contig name to its length (int)
+		p: exponent for cost computation
 	Returns:
 		Total of (length of contig sets)^p and the cost of partitioning
 	'''	
@@ -147,8 +153,7 @@ def get_partition_cost(partitions, contigs_dict, p):
 	for S in partitions:
 		S_len = 0
 		for contig in S:
-			contig_len = contigs_dict[contig.split('_')[0]]['length']
-			S_len += contig_len
+			S_len += ctg_len[contig]
 		if S_len != 0:
 			S_cost = S_len**p
 			total_len += S_cost
@@ -156,7 +161,7 @@ def get_partition_cost(partitions, contigs_dict, p):
 	cost = total_len - largest_part_cost
 	return total_len, cost
 
-def compute_splits_cost(pls_ids, side_contig_copies, opp_contig_copies, B, flag, pls_ids_dict, contigs_dict, p):
+def compute_splits_cost(pls_ids, side_contig_copies, opp_contig_copies, B, flag, pls_ids_dict, ctg_len, p):
 	'''
 	Input:
 		pls_ids: List of plasmid ids,
@@ -191,12 +196,12 @@ def compute_splits_cost(pls_ids, side_contig_copies, opp_contig_copies, B, flag,
 				side_contigs, opp_contigs = set(side_ctgs_by_pls[edge[flag]]), set(opp_ctgs_by_pls[edge[1-flag]])
 				common = side_contigs.intersection(opp_contigs)
 				partitions = modify_partitions(partitions, common)
-		node_len, cost = get_partition_cost(partitions,contigs_dict,p)
+		node_len, cost = get_partition_cost(partitions, ctg_len, p)
 		side_len += node_len
 		side_cost += cost
 	return side_cost
 
-def compute_match_cost(left_contig_copies, right_contig_copies, pls_ids_dict, contigs_dict, p):
+def compute_match_cost(left_contig_copies, right_contig_copies, pls_ids_dict, ctg_len, p):
 	'''
 	Input:
 		List of contig copies, one for each side
@@ -216,8 +221,8 @@ def compute_match_cost(left_contig_copies, right_contig_copies, pls_ids_dict, co
 		left_pls_ids, right_pls_ids = nx.bipartite.sets(C)		#Split the component according to bipartite sets
 		if len(list(right_pls_ids)) != 0 and list(right_pls_ids)[0] in left_pls_set: 	#Ensuring proper assignments of bipartite parts
 			right_pls_ids,left_pls_ids = left_pls_ids,right_pls_ids
-		left_splits_cost += compute_splits_cost(left_pls_ids, left_contig_copies, right_contig_copies, B, 0, pls_ids_dict, contigs_dict, p)
-		right_splits_cost += compute_splits_cost(right_pls_ids, right_contig_copies, left_contig_copies, B, 1, pls_ids_dict, contigs_dict, p)
+		left_splits_cost += compute_splits_cost(left_pls_ids, left_contig_copies, right_contig_copies, B, 0, pls_ids_dict, ctg_len, p)
+		right_splits_cost += compute_splits_cost(right_pls_ids, right_contig_copies, left_contig_copies, B, 1, pls_ids_dict, ctg_len, p)
 
 	return left_splits_cost, right_splits_cost	
 
@@ -231,13 +236,13 @@ def compute_current_cost(matching_dict, pls_ids_dict, contigs_dict, p):
 	Returns:
 		Cost of current matching
 	'''	
-	left_contig_copies, right_contig_copies = rename_by_matching(matching_dict)
+	left_contig_copies, right_contig_copies, ctg_len = rename_by_matching(matching_dict, contigs_dict)
 	left_ctg_ids, right_ctg_ids = set(), set()
 	for x in left_contig_copies:
 		left_ctg_ids.add(x[0])
 	for x in right_contig_copies:
 		right_ctg_ids.add(x[0])
-	return compute_match_cost(left_contig_copies, right_contig_copies, pls_ids_dict, contigs_dict, p)
+	return compute_match_cost(left_contig_copies, right_contig_copies, pls_ids_dict, ctg_len, p)
 
 def run_compare_plasmids(contigs_dict, pls_ids_dict, p, max_calls, results_file):
 	'''
